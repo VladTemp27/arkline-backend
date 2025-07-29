10e2d95041c8d8428fc541252f0b3c16f0daf7ce
Lestat <2233915@slu.edu.ph>
2025-07-30 04:51:44 +0800

    feat: add create-user route to authentication service

--- user-service/routes/auth-route.js
+++ user-service/routes/auth-route.js
@@ -1,10 +1,11 @@
 import express from 'express';
 
-import { handleAuth } from './controller/auth-controller.js';
+import { handleAuth, handleCreateUser } from '../controller/auth-controller.js';
 
 const authRouter = express.Router();
 authRouter.use(express.json());
 
 authRouter.post('/authenticate', handleAuth);
+authRouter.post('/create-user', handleCreateUser); // secure this route later
 
 export { authRouter };
