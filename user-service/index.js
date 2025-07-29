92cd3107f0aec69ddcc362ffc0ff234276224433
Lestat <2233915@slu.edu.ph>
2025-07-30 04:52:05 +0800

    feat: refactor user service initialization and update health check response

--- user-service/index.js
+++ user-service/index.js
@@ -1,6 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
+import { connectMongoDB, disconnectMongoDB } from './util/mongo-util.js';
+import { connectRedis, redisClient } from './util/redis-util.js';
+
+import { authRouter } from './routes/auth-route.js';
+
 dotenv.config({path: './.env'})
 
 const PORT = process.env.PORT || 3000;
@@ -9,15 +14,20 @@ const app = express();
 app.use(express.json());
 
 app.get('/health', (req, res) => {
-    res.status(200).json({message: 'Auth service is running'}); 
+    res.status(200).json({message: 'User service is running'}); 
 })
 
-app.use('/login')
+app.use('/auth', authRouter);
 
 try{
+    connectMongoDB();
+    connectRedis();
     app.listen(PORT, () => {
-        console.log(`Auth service is running on port ${PORT}`);
+        console.log(`User service is running on port ${PORT}`);
     });
 }catch(error){
     console.error('Error starting the server:', error);
+    disconnectMongoDB();
+    redisClient.quit();
+    process.exit(1);
 }
