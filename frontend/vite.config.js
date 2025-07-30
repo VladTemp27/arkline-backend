012c39440a59cdc9e478f854d04da2c711d50cc9
Sean <sdaoctavo@gmail.com>
2025-07-30 15:58:48 +0800

    feat: enhance Vite configuration with improved proxy logging for API requests

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,37 @@
-import path from "path"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 import tailwindcss from '@tailwindcss/vite'
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
-   resolve: {
+  plugins: [react(), tailwindcss()],
+  resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  server: {
+    proxy: {
+      '/api/accomplishment-tracking': {
+        target: 'http://localhost:2010',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api\/accomplishment-tracking/, ''),
+        configure: (proxy, options) => {
+          proxy.on('proxyReq', (proxyReq, req, res) => {
+            console.log('Sending Request to the Target:', req.method, req.url);
+          });
+          proxy.on('proxyRes', (proxyRes, req, res) => {
+            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+          });
+        },
+      },
+      '/api/user-service': {
+        target: 'http://localhost:2020',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api\/user-service/, ''),
+      }
+    }
+  }
+});
